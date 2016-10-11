988aa5e7409a6355eea6fe65c12f179c163316e6
JoseB <lmj@iesseveroochoa.net>
2016-10-11 12:02:32 +0200

    Initial commit

---
+++ app/src/main/java/net/iessochoa/joseantoniolopez/startactivityforresult/EntradaDatosActivity.java
@@ -0,0 +1,55 @@
+package net.iessochoa.joseantoniolopez.startactivityforresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EntradaDatosActivity extends AppCompatActivity implements View.OnClickListener {
+    //Constantes para identificar los valores Extra que se envían a la actividad EntradaDatos
+    //Es constumbre identificarlas mediante el nombre del paquete
+    public final static String EXTRA_DATOS_RESULTADO ="net.iessochoa.joseantoniolopez.startactivityforresult.entradadatosactivity.datos";
+    EditText et_datos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrada_datos);
+
+        et_datos=(EditText) findViewById(R.id.et_datos);
+
+        //Manejaremos el evento del botón en el mismo Listener
+        ((Button)findViewById(R.id.btn_Ok)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_Cancel)).setOnClickListener(this);
+
+        //obtenemos el valor de la actividad llamadora y lo mostraremos
+        et_datos.setText(getIntent().getStringExtra(MainActivity.EXTRA_DATOS));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_Ok:
+                // Si el EditText no está vacío enviamos el resultado
+                if(et_datos.getText().length()!=0) {
+                    //guardamos el resultado en el Intent que llamó la actividad, autque
+                    //podríamos crear uno nuevo
+                    Intent iBack = getIntent();
+
+                    iBack.putExtra(EXTRA_DATOS_RESULTADO,et_datos.getText().toString());
+                    //indicamos que se ha pulsado aceptar y enviamos el Intent
+                    setResult(RESULT_OK,iBack);
+                    //cerramos la actividad
+                    finish();
+                }
+                break;
+            case R.id.btn_Cancel:
+                //Indicamos que el usuario ha pulsado Cancelar
+                setResult(RESULT_CANCELED);
+                //cerramos la actividad
+                finish();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/net/iessochoa/joseantoniolopez/startactivityforresult/MainActivity.java
@@ -0,0 +1,95 @@
+package net.iessochoa.joseantoniolopez.startactivityforresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //Estas constantes nos permitirán distiguir para qué llamamos la actividad EntradaDatos
+    //en el evento onActivityResult
+    public final static int OPTION_REQUEST_NOMBRE=0;
+    public final static int OPTION_REQUEST_APELLIDO=1;
+
+    //Constantes para identificar los valores Extra que se envían a la actividad EntradaDatos
+    //Es constumbre identificarlas mediante el nombre del paquete
+    public final static String EXTRA_DATOS="net.iessochoa.joseantoniolopez.startactivityforresult.main.datos";
+
+
+    TextView tv_Nombre;
+    TextView tv_Apellido;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_Nombre=(TextView) findViewById(R.id.tv_Nombre);
+        tv_Apellido=(TextView) findViewById(R.id.tv_Apellido);
+
+        //Manejaremos el evento del botón en el mismo Listener
+        ((Button)findViewById(R.id.btn_Nombre)).setOnClickListener(this);
+        ((Button)findViewById(R.id.btn_Apellido)).setOnClickListener(this);
+
+    }
+
+    /**
+     * Maneja el click de los botones de la Activity
+     * @param view: Boton que recibe el click
+     */
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        //creamos el intent para llamar a la acitividad
+        i=new Intent(this,EntradaDatosActivity.class);
+        switch (view.getId()) {
+            case R.id.btn_Nombre:
+                //enviamos el nombre actual
+                i.putExtra(EXTRA_DATOS,tv_Nombre.getText().toString());
+                //llamamos a la actividad a la espera de recibir el resultado
+                //indicando el código de llamada
+                startActivityForResult(i,OPTION_REQUEST_NOMBRE);
+                break;
+            case R.id.btn_Apellido:
+                //enviamos el apellido actual
+                i.putExtra(EXTRA_DATOS,tv_Apellido.getText().toString());
+                //llamamos a la actividad a la espera de recibir el resultado
+                //indicando el código de llamada
+                startActivityForResult(i,OPTION_REQUEST_APELLIDO);
+                break;
+        }
+    }
+
+    /**
+     * Cuando la actividad EntradaDatos termine, esta actividad llamará a este evento, donde
+     * podremos obtener el resultado devuelto.
+     * @param requestCode: si fue llamada para NOMBRE on APELLIDO
+     * @param resultCode: Si el usuario pulsó ACEPTAR o CANCELAR
+     * @param data: Datos devueltos
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Comprobamos si el resultado de la segunda actividad no es  "RESULT_CANCELED".
+        if (resultCode != RESULT_CANCELED) {
+            // De lo contrario, recogemos el resultado de la segunda actividad.
+            String resultado = data.getStringExtra(EntradaDatosActivity.EXTRA_DATOS_RESULTADO);
+            // Y tratamos el resultado en función de si se lanzó para rellenar el
+            // nombre o el apellido.
+            switch (requestCode) {
+                case OPTION_REQUEST_NOMBRE:
+                    tv_Nombre.setText(resultado);
+                    break;
+                case OPTION_REQUEST_APELLIDO:
+                    tv_Apellido.setText(resultado);
+                    break;
+
+            }
+        }
+    }
+
+
+}
